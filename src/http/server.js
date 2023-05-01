9cf4408c21f8ce744639ea8d0bcdee40c08bad7a
Hetakanen <applepiefever$@gmail.com>
2023-05-01 19:08:35 +0300

    chore: Add cors config for http

--- src/http/server.js
+++ src/http/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { createHandler } from 'graphql-http/lib/use/express';
 import schema from './schema.js';
+import cors from 'cors';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
+app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
