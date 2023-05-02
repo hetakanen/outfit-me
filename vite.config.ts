2eeac252ad0bbc55c69d64174447557dc371ea85
Hetakanen <applepiefever$@gmail.com>
2023-05-02 19:43:35 +0300

    chore: Add bundling of file

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { terser } from 'rollup-plugin-terser';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    terser() // minify the output
+  ],
   build: {
-    manifest: true,
+    outDir: 'dist',
+    assetsInlineLimit: 0,
     rollupOptions: {
       input: "./src/client/main.tsx",
-    },
-  },
+      output: {
+        dir: './dist',
+        format: 'es'
+      }
+    }
+  }
 });
